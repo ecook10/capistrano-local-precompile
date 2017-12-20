9ab951a03f0eb8366b75a0c85ff8c958ffaa724f
Evan Cook <evancookie2@gmail.com>
2017-12-19 19:25:47 -0800

    use ssh options hash to find name

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -54,7 +54,7 @@ namespace :deploy do
           #local_manifest_path = execute "ls #{fetch(:assets_dir)}/manifest*"
           #local_manifest_path.strip!
 
-          host = "#{server.properties.name}@#{server.hostname}"
+          host = "#{server.netssh_options[:user]}@#{server.hostname}"
           execute "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{host}:#{fetch(:release_path)}/#{fetch(:assets_dir)}/"
           execute "#{fetch(:rsync_cmd)} ./#{fetch(:packs_dir)}/ #{host}:#{fetch(:release_path)}/#{fetch(:packs_dir)}/"  #TODO: Check if exists
           #execute "#{fetch(:rsync_cmd)} ./#{local_manifest_path} #{user}@#{server}:#{release_path}/assets_manifest#{File.extname(local_manifest_path)}"
